ec78adf402186a9c904c06b590bd464e6ae9eee2
George Fouche <fouliex@gmail.com>
2020-08-29 22:23:17 -0700

    setup upload list and download

--- src/dev/service/csv_web_service.py
+++ src/dev/service/csv_web_service.py
@@ -1,9 +1,11 @@
 import logging
+import os
 import io
 import csv
-from flask import request
+from flask import request, send_from_directory
 from flask_restful import Resource
 from dev.model.csv_web_app_model import CsvWebAppModel
+from dev.util.helper.get_config import get_config_value
 
 
 class Online(Resource):
@@ -19,10 +21,32 @@ class CsvWebGetFilesService(Resource):
         data_list = []
         model = CsvWebAppModel.find_all()
         for data in model:
-            print(data.file_name)
-            var = {"name": data.file_name, "url": "http://localhost:8087/files/{}".format(data.file_name)}
+            var = {"name": data.filename, "url": "http://localhost:5003/csvwebapp/files/{}".format(data.filename)}
             data_list.append(var)
         return data_list
+
+
+class CsvWebDownloadService(Resource):
+    @classmethod
+    def get(cls, filename):
+        print(filename)
+        model = CsvWebAppModel.find_by_filename(filename)
+        dict_data = model.csv_data
+        csv_columns = dict_data['csv_data'][0].keys()
+        path = get_config_value('temp_download_folder')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        csv_file = filename
+        try:
+            with open(os.path.join(path, csv_file), 'w+') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in dict_data['csv_data']:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
+        # send the buffer as a regular file
+        return send_from_directory('../../temp', filename, as_attachment=True)
 
 
 class CsvWebUploadService(Resource):
@@ -36,13 +60,11 @@ class CsvWebUploadService(Resource):
         filename = f.filename
         content_type = f.content_type
         stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-        csv_input = csv.reader(stream)
-        iterip = iter(csv_input)
-        next(iterip)
-        csv_data = []
-        for row in iterip:
-            csv_data.append(row)
-        csv_data_dict = {'csv_data': csv_data}
+        reader = csv.DictReader(stream)
+        data_list = []
+        for row in reader:
+            data_list.append(row)
+        csv_data_dict = {'csv_data': data_list}
         model = CsvWebAppModel(filename, content_type, csv_data_dict)
         model.save_to_db()
         dict_message = {'message': 'Update the file successfully: {}'.format(filename)}
