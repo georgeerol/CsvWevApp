bbd330f0ebaf42acfea4c2f19e809642be3d39f5
George E Fouche <fouliex@gmail.com>
2020-09-01 10:01:24 -0700

    Feature better display (#3)
    * set up display csv api
    
    * Working display csv
    
    * fix display formatting
    
    * add small examples

--- frontend/src/components/csvweb/CsvWebComponent.jsx
+++ frontend/src/components/csvweb/CsvWebComponent.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from "react";
+import {Column, Table} from "react-virtualized";
+import 'react-virtualized/styles.css'; // only needs to be imported once
+import Draggable from "react-draggable";
 import UploadService from "../../service/UploadFilesService"
+
+const TOTAL_WIDTH = 1900;
 
 
 class CsvWebComponent extends Component {
@@ -7,14 +12,27 @@ class CsvWebComponent extends Component {
         super(props);
         this.selectFile = this.selectFile.bind(this);
         this.upload = this.upload.bind(this);
+        this.displayClicked = this.displayClicked.bind(this);
+        this.getKeys = this.getKeys.bind(this);
+        this.getHeader = this.getHeader.bind(this);
+        this.getRowsData = this.getRowsData.bind(this);
+
 
         this.state = {
             selectedFiles: undefined,
             currentFile: undefined,
+            display_csv_data: undefined,
+            isDisplayActive: false,
+            hasMore: true,
             progress: 0,
             message: "",
-
             fileInfos: [],
+
+            widths: {
+                name: 0.33,
+                location: 0.33,
+                description: 0.33
+            }
         };
     }
 
@@ -26,15 +44,59 @@ class CsvWebComponent extends Component {
         });
     }
 
+    handleShow = () => {
+        const {isDisplayActive} = this.state;
+        this.setState({
+            isDisplayActive: !isDisplayActive,
+        })
+    };
+
+    displayClicked(filename) {
+        console.log(filename);
+        UploadService.displayFile(filename).then(response => {
+            this.setState({display_csv_data: response.data});
+
+        }).then(() => {
+            this.handleShow();
+            console.log("LALA LAND");
+            console.log(this.state.display_csv_data.csv_data)
+        }).catch(() => {
+
+        });
+
+    }
+
+    getKeys = function () {
+        console.log("Stephane");
+        console.log(this.state.display_csv_data.csv_data[0]);
+        return Object.keys(this.state.display_csv_data.csv_data[0]);
+    };
+
+    getHeader = function () {
+        var keys = this.getKeys();
+        return keys.map((key, index) => {
+            return <th key={key}>{key.toUpperCase()}</th>
+        })
+    };
+
+    getRowsData = function () {
+        var items = this.state.display_csv_data.csv_data;
+        var keys = this.getKeys();
+        return items.map((row, index) => {
+            return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+        });
+    };
+
+
     selectFile(event) {
         this.setState({
             selectedFiles: event.target.files,
         });
     }
 
+
     upload() {
         let currentFile = this.state.selectedFiles[0];
-
         this.setState({
             progress: 0,
             currentFile: currentFile,
@@ -43,6 +105,7 @@ class CsvWebComponent extends Component {
         UploadService.upload(currentFile, (event) => {
             this.setState({
                 progress: Math.round((100 * event.loaded) / event.total),
+
             });
         })
             .then((response) => {
@@ -69,17 +132,146 @@ class CsvWebComponent extends Component {
         });
     }
 
+
     render() {
         const {
             selectedFiles,
             currentFile,
             progress,
             message,
+            widths,
             fileInfos,
         } = this.state;
 
+
         return (
+
             <div>
+                <h1> Csv Web Application </h1>
+
+                <label className="btn btn-default">
+                    <input type="file" onChange={this.selectFile}/>
+                </label>
+
+                <button
+                    className="btn btn-success"
+                    disabled={!selectedFiles}
+                    onClick={this.upload}
+                >Upload
+                </button>
+
+                <div className="alert alert-light" role="alert">
+                    {message}
+                </div>
+
+                <div className="container">
+                    <div className="card-header">List of CSVs</div>
+                    <table className="table">
+                        <tbody>
+                        {fileInfos &&
+                        fileInfos.map((file, index) => (
+                            <tr key={index}>
+                                <td><a href={file.url}>{file.name}</a></td>
+                                <td>
+                                    <button className="btn btn-success" onClick={() => this.displayClicked(file.name)}>
+                                        Display
+                                    </button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-warning">
+                                        Date Statistics
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+
+                </div>
+
+                {this.state.isDisplayActive ?
+
+
+                    <Table
+                        width={TOTAL_WIDTH}
+                        height={300}
+                        headerHeight={20}
+                        rowHeight={30}
+                        rowCount={this.state.display_csv_data.csv_data.length}
+                        rowGetter={({index}) => this.state.display_csv_data.csv_data[index]}
+                    >
+                        <Column
+                            headerRenderer={this.headerRenderer}
+                            dataKey="guid"
+                            label="Guid"
+                            width={2 * TOTAL_WIDTH}
+                        />
+
+                        <Column
+                            headerRenderer={this.headerRenderer}
+                            dataKey="name"
+                            label="Name"
+                            width={TOTAL_WIDTH/2}
+                        />
+
+                        <Column
+                            dataKey="first"
+                            label="First"
+                            width={TOTAL_WIDTH/2}
+                        />
+
+                        <Column
+                            dataKey="last"
+                            label="Last"
+                            width={TOTAL_WIDTH/2}
+                        />
+
+                        <Column
+                            dataKey="email"
+                            label="Email"
+                            width={TOTAL_WIDTH}
+                        />
+
+                        <Column
+                            dataKey="value"
+                            label="Value"
+                            width={TOTAL_WIDTH/3}
+                        />
+
+                        <Column
+                            dataKey="date"
+                            label="Date"
+                            width={TOTAL_WIDTH}
+                        />
+
+                        <Column
+                            dataKey="phone"
+                            label="Phone"
+                            width={TOTAL_WIDTH}
+                        />
+
+                        <Column
+                            dataKey="age"
+                            label="Age"
+                            width={TOTAL_WIDTH/5}
+                        />
+
+                        <Column
+                            dataKey="state"
+                            label="State"
+                            width={TOTAL_WIDTH/4}
+                        />
+
+                        <Column
+                            dataKey="street"
+                            label="Street"
+                            width={TOTAL_WIDTH}
+                        />
+
+
+                    </Table>
+                    : null
+                }
                 {currentFile && (
                     <div className="progress">
                         <div
@@ -95,38 +287,16 @@ class CsvWebComponent extends Component {
                     </div>
                 )}
 
-                <label className="btn btn-default">
-                    <input type="file" onChange={this.selectFile}/>
-                </label>
-
-                <button
-                    className="btn btn-success"
-                    disabled={!selectedFiles}
-                    onClick={this.upload}
-                >
-                    Upload
-                </button>
-
-                <div className="alert alert-light" role="alert">
-                    {message}
-                </div>
-
-                <div className="card">
-                    <div className="card-header">List of Files</div>
-                    <ul className="list-group list-group-flush">
-                        {fileInfos &&
-                        fileInfos.map((file, index) => (
-                            <li className="list-group-item" key={index}>
-                                <a href={file.url}>{file.name}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
             </div>
         );
     }
 
-
 }
+
+const RenderRow = (props) => {
+    return props.keys.map((key, index) => {
+        return <td key={props.data[key]}>{props.data[key]}</td>
+    })
+};
 
 export default CsvWebComponent;
--- src/app.py
+++ src/app.py
@@ -6,7 +6,8 @@ from flask_restful import Api
 from dev.env.base_env import app
 from dev.env.dev_postgres_db import DevPostgresDB
 from dev.env.dev_sqllite_db import DevSqlLiteDB
-from dev.service.csv_web_service import CsvWebUploadService, CsvWebGetFilesService, CsvWebDownloadService
+from dev.service.csv_web_service import CsvWebUploadService, CsvWebGetFilesService, CsvWebDownloadService, \
+    CsvWebDisplayService
 from dev.service.csv_web_service import Online
 from dev.util.helper.get_config import get_config_value
 
@@ -25,6 +26,7 @@ flask_api = Api(app)
 flask_api.add_resource(CsvWebUploadService, get_config_value('upload_file_url_path'))
 flask_api.add_resource(CsvWebGetFilesService, get_config_value('get_list_of_files'))
 flask_api.add_resource(CsvWebDownloadService, get_config_value('download_a_file'))
+flask_api.add_resource(CsvWebDisplayService, get_config_value('display_a_file'))
 flask_api.add_resource(Online, '/')
 
 if __name__ == "__main__":
