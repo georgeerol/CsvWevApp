6f77764599c83770c9169b295858d3b0a2d14ff5
George Fouche <fouliex@gmail.com>
2020-09-01 15:55:48 -0700

    Update model to one to many relationship

---
+++ src/dev/model/csv_web_model.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from dev.db.db import db
+
+
+class CsvWebAppFileModel(db.Model):
+    __tablename__ = 'csv_web_app_file'
+
+    filename = db.Column(db.String(), primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    content_type = db.Column(db.String())
+    csv_data = db.relationship('CsvWebAppCsvModel')
+
+    def __init__(self, filename, content_type, csv_data):
+        self.filename = filename
+        self.content_type = content_type
+        self.csv_data = csv_data
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_filename(cls, filename):
+        return cls.query.filter_by(filename=filename).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class CsvWebAppCsvModel(db.Model):
+    __tablename__ = 'csv_web_app_csv'
+
+    guid = db.Column(db.String(), primary_key=True)
+    filename_id = db.Column(db.String, db.ForeignKey('csv_web_app_file.filename'))
+    name = db.Column(db.String)
+    first = db.Column(db.String)
+    last = db.Column(db.String)
+    email = db.Column(db.String)
+    value = db.Column(db.String)
+    date = db.Column(db.String)
+    phone = db.Column(db.String)
+    age = db.Column(db.String)
+    state = db.Column(db.String)
+    street = db.Column(db.String)
+
+    def __init__(self, guid, name, first, last, email, value, date, phone, age, state, street):
+        self.guid = guid
+        self.name = name
+        self.first = first
+        self.last = last
+        self.email = email
+        self.value = value
+        self.date = date
+        self.phone = phone
+        self.age = age
+        self.state = state
+        self.street = street
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- src/dev/service/csv_web_service.py
+++ src/dev/service/csv_web_service.py
@@ -4,7 +4,7 @@ import io
 import csv
 from flask import request, send_from_directory
 from flask_restful import Resource
-from dev.model.csv_web_app_model import CsvWebAppModel
+from dev.model.csv_web_model import CsvWebAppFileModel, CsvWebAppCsvModel
 from dev.util.helper.get_config import get_config_value
 
 
@@ -19,7 +19,7 @@ class CsvWebGetFilesService(Resource):
     @classmethod
     def get(cls):
         data_list = []
-        model = CsvWebAppModel.find_all()
+        model = CsvWebAppFileModel.find_all()
         for data in model:
             var = {"name": data.filename, "url": "http://localhost:5003/csvwebapp/files/{}".format(data.filename)}
             data_list.append(var)
@@ -29,7 +29,7 @@ class CsvWebGetFilesService(Resource):
 class CsvWebDisplayService(Resource):
     @classmethod
     def get(cls, filename):
-        model = CsvWebAppModel.find_by_filename(filename)
+        model = CsvWebAppFileModel.find_by_filename(filename)
         dict_data = model.csv_data
         return dict_data
 
@@ -37,8 +37,7 @@ class CsvWebDisplayService(Resource):
 class CsvWebDownloadService(Resource):
     @classmethod
     def get(cls, filename):
-        print(filename)
-        model = CsvWebAppModel.find_by_filename(filename)
+        model = CsvWebAppFileModel.find_by_filename(filename)
         dict_data = model.csv_data
         csv_columns = dict_data['csv_data'][0].keys()
         path = get_config_value('temp_download_folder')
@@ -70,10 +69,16 @@ class CsvWebUploadService(Resource):
         stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
         reader = csv.DictReader(stream)
         data_list = []
+
         for row in reader:
-            data_list.append(row)
-        csv_data_dict = {'csv_data': data_list}
-        model = CsvWebAppModel(filename, content_type, csv_data_dict)
+            print('george')
+            # guid, name, first, last, email, value, date, phone, age, state, street
+            csv_data = CsvWebAppCsvModel(row['guid'], row['name'], row['first'], row['last'], row['email'],
+                                         row['value'], row['date'], row['phone'], row['age'], row['state'],
+                                         row['street'])
+            data_list.append(csv_data)
+        # csv_data_dict = {'csv_data': data_list}
+        model = CsvWebAppFileModel(filename, content_type, data_list)
         model.save_to_db()
         dict_message = {'message': 'Update the file successfully: {}'.format(filename)}
         return dict_message
