210329fc1e1e7e681bccbdf2856d3257f5a014e4
George Fouche <fouliex@gmail.com>
2020-09-07 17:26:09 -0700

    Add test file

--- src/test/integration/mgr/upload_mgr_test.py
+++ src/test/integration/mgr/upload_mgr_test.py
@@ -8,10 +8,10 @@ class UploadFileManagerTest(BaseTest):
 
     def test_process_file(self):
         with self.app_context():
-            filename = 'test.csv'
+            filename = 'upload_test.csv'
             test_file = get_config_value('temp_download_folder') + '/' + filename
 
             file_to_Upload = FileStorage(stream=open(test_file, "rb"), filename=filename, content_type='text/csv')
             upload_mgr = UploadCsvManager(file_to_Upload)
             actual_response = upload_mgr.save_to_db()
-            self.assertEqual(({'message': 'Upload example_small.csv Successfully'}, 201), actual_response)
+            self.assertEqual(({'message': 'Upload {} Successfully'.format(filename)}, 201), actual_response)
